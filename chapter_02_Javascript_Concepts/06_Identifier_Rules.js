d7df6fb77ab1f45756eaa93682a9f5da102b2465
ManshiDesai <manshi.desai@gmail.com>
2026-05-28 19:06:02 +0530

    commit javascript files

---
+++ chapter_02_Javascript_Concepts/06_Identifier_Rules.js
@@ -0,0 +1,12 @@
+var $ = 10;
+var _a = 12;
+var p = 10;
+
+var ab123 = 34;
+var Name = "pramod";
+var Name = "Amit";
+
+
+var pramod_dutta = "Hello";
+var pramod$dutta = "Hello";
+var pramod1232 = "Hello";
---
+++ chapter_02_Javascript_Concepts/07_Identifier_Part2.js
@@ -0,0 +1,7 @@
+var name = "Pramod";
+
+var firstName = "Pramod";
+var lastName = "Dutta";
+// Camel case
+var first_name = "Amit"; // Snake case
+
