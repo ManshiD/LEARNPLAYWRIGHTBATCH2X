80e1d7226d651ed5581f6e60dd84c9a551c0c1b4
ManshiDesai <manshi.desai@gmail.com>
2026-05-31 15:08:54 +0530

    commit chapter 10 Loops

---
+++ Chapter_10_Loops/74_IQ.js
@@ -0,0 +1,21 @@
+for (let promod = 0; promod > 1; promod++){
+    console.log(promod);
+}
+
+/* INFINITE LOOP EXAMPLE
+for (let promod = 0; ; promod++){
+    console.log(promod);
+}
+*/
+
+for (let somya = 0; somya < 18; somya++){
+    if (somya > 15) {
+        console.log("Gift from Papa, iphone this year");
+    } else {
+        console.log("No Gift, iphone only barbie doll");
+    }
+}
+
+for (let i=0; i<1; i++){
+    console.log(i);
+}
---
+++ Chapter_10_Loops/81_IQ.js
@@ -0,0 +1,4 @@
+for (let i=0; i < 3; i++){
+    if (i === 1) continue;  // Skip the rest of the loop body when i is 1
+    console.log(i);
+}
